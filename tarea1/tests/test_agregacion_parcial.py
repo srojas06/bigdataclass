ad1438aa511b168dc14f58596c909dc004752b8e
srojas06 <57544567+srojas06@users.noreply.github.com>
2024-10-10 06:05:42 -0600

    Update test_agregacion_parcial.py

--- tarea1/tests/test_agregacion_parcial.py
+++ tarea1/tests/test_agregacion_parcial.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import sum as _sum, count, min as _min, max as _max
+from pyspark.sql.functions import sum as _sum, count, min as _min, max as _max, avg as _avg
 import pytest
 
 @pytest.fixture(scope="session")
