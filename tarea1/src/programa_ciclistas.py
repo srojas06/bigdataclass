22ecd5925102d0e42e6e014a59877c1c4d0945d9
srojas06 <57544567+srojas06@users.noreply.github.com>
2024-10-10 02:52:01 -0600

    Update programa_ciclistas.py

--- tarea1/src/programa_ciclistas.py
+++ tarea1/src/programa_ciclistas.py
@@ -2,10 +2,10 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import sum as _sum, countDistinct, col, rank, round
 from pyspark.sql.window import Window
 
-# Crea la sesión de Spark
+
 spark = SparkSession.builder.appName("Ciclistas_Top5").getOrCreate()
 
-# Cargar los CSV
+# Carga los CSV 
 df_ciclistas = spark.read.csv('/src/archivo/ciclista.csv', header=False, inferSchema=True)\
     .toDF('Cedula', 'Nombre', 'Provincia')
 
@@ -15,36 +15,33 @@ df_rutas = spark.read.csv('/src/archivo/ruta.csv', header=False, inferSchema=Tru
 df_actividades = spark.read.csv('/src/archivo/actividad.csv', header=False, inferSchema=True)\
     .toDF('Codigo_Ruta', 'Cedula', 'Fecha')
 
-# Verificar cuántos ciclistas de Alajuela hay en total
-df_ciclistas.filter(col("Provincia") == "Alajuela").show()
+# Quita los espacios en blanco 
+df_ciclistas = df_ciclistas.withColumn('Cedula', col('Cedula').cast('string').trim())
+df_ciclistas = df_ciclistas.withColumn('Nombre', col('Nombre').trim())
 
-# Verificar cuántos ciclistas de Alajuela tienen actividades
-df_ciclistas_actividades = df_actividades.join(df_ciclistas, 'Cedula')
-df_ciclistas_actividades.filter(col("Provincia") == "Alajuela").groupBy("Cedula", "Nombre").count().show()
-
-# Unir los datos
+# Une los datos
 df_merged = df_actividades.join(df_ciclistas, 'Cedula')\
                           .join(df_rutas, 'Codigo_Ruta')
 
-# Calcular los km totales por ciclista
+# Calcula los km totales por cada ciclista
 df_total_km = df_merged.groupBy('Cedula', 'Nombre', 'Provincia')\
                        .agg(_sum('Kilometros').alias('Kilometros_Totales'))
 
-# Calcular los días activos
+# Calcular los dias en las que hay actividad
 df_dias_actividades = df_merged.groupBy('Cedula', 'Nombre', 'Provincia')\
                                .agg(countDistinct('Fecha').alias('Dias_Activos'))
 
-# Unir las tablas con kilómetros totales y días activos
+# Une las tablas con km totales y dias donde haya actividad 
 df_final = df_total_km.join(df_dias_actividades, ['Cedula', 'Nombre', 'Provincia'])\
                       .withColumn('Promedio_Diario', round(col('Kilometros_Totales') / col('Dias_Activos'), 2))
 
 # Crear una ventana de partición por provincia para el ranking
 windowSpec = Window.partitionBy('Provincia').orderBy(col('Kilometros_Totales').desc(), col('Promedio_Diario').desc())
 
-# Obtener el top 5 de ciclistas por provincia
+# Obtiene el top 5 de ciclistas por provincia
 df_top_5 = df_final.withColumn("rank", rank().over(windowSpec))\
                    .filter(col("rank") <= 5)\
                    .drop("rank")
 
-# Mostrar el top 5 de ciclistas por provincia
+# Muestra el top 5 de ciclistas por provincia
 df_top_5.show(35)
