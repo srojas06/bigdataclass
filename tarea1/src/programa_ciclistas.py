07323caf6dc4294566bacc7cfed0a0673c744479
srojas06 <57544567+srojas06@users.noreply.github.com>
2024-10-09 22:58:10 -0600

    Update programa_ciclistas.py

--- tarea1/src/programa_ciclistas.py
+++ tarea1/src/programa_ciclistas.py
@@ -3,11 +3,11 @@ from pyspark.sql.functions import sum as _sum, countDistinct, col
 from pyspark.sql.window import Window
 from pyspark.sql.functions import rank
 
-# Crea la sesión de Spark
-spark = SparkSession.builder.appName("Ciclistas_Top5").getOrCreate()
 
-# Carga los CSV 
-def carga_datos():
+spark = SparkSession.builder.appName("Top5_Ciclistas_Provincia").getOrCreate()
+
+
+def cargar_datos():
     df_ciclistas = spark.read.csv('/src/archivo/ciclista.csv', header=False, inferSchema=True)\
         .toDF('Cedula', 'Nombre', 'Provincia')
     df_rutas = spark.read.csv('/src/archivo/ruta.csv', header=False, inferSchema=True)\
@@ -17,59 +17,57 @@ def carga_datos():
     return df_ciclistas, df_rutas, df_actividades
 
 
-# Une los datos
-def union_datos(df_ciclistas, df_rutas, df_actividades):
-    df_merged = df_actividades.join(df_ciclistas, 'Cedula')\
-                              .join(df_rutas, 'Codigo_Ruta')
-    return df_merged
+def unir_datos(df_ciclistas, df_rutas, df_actividades):
+    df_union = df_actividades.join(df_ciclistas, 'Cedula')\
+                             .join(df_rutas, 'Codigo_Ruta')
+    return df_union
 
-# Calcula los km totales
-def calcula_kilometros(df_merged):
-    df_total_km = df_merged.groupBy('Cedula', 'Nombre', 'Provincia')\
-                           .agg(_sum('Kilometros').alias('Kilometros_Totales'))
+
+def calcular_kilometros_totales(df_union):
+    df_total_km = df_union.groupBy('Cedula', 'Nombre', 'Provincia')\
+                          .agg(_sum('Kilometros').alias('Kilometros_Totales'))
     return df_total_km
 
-# Calcula el promedio diario
-def calcula_promedio(df_merged):
-    df_dias_actividades = df_merged.groupBy('Cedula', 'Nombre', 'Provincia')\
-                                   .agg(countDistinct('Fecha').alias('Dias_Activos'))
-    return df_dias_actividades
 
-# Obtiene el top 5 de ciclistas por cada provincia
-def top_5_ciclistas(df_final, criterio):
+def calcular_promedio_diario(df_union):
+    df_dias_activos = df_union.groupBy('Cedula', 'Nombre', 'Provincia')\
+                              .agg(countDistinct('Fecha').alias('Dias_Activos'))
+    return df_dias_activos
+
+def obtener_top_5(df_final, criterio):
     windowSpec = Window.partitionBy('Provincia').orderBy(col(criterio).desc())
     df_top_5 = df_final.withColumn("rank", rank().over(windowSpec))\
                        .filter(col("rank") <= 5)\
                        .drop("rank")
     return df_top_5
 
-# Programa principal
+
 def main():
-    # Carga los datos
-    df_ciclistas, df_rutas, df_actividades = carga_datos()
-  
-    # Une los datos
-    df_merged = union_datos(df_ciclistas, df_rutas, df_actividades)
+    
+    df_ciclistas, df_rutas, df_actividades = cargar_datos()
 
-    # Calcula los km totales
-    df_total_km = calcula_kilometros(df_merged)
+    # se une los datos
+    df_union = unir_datos(df_ciclistas, df_rutas, df_actividades)
 
-    # Calcular el promedio diario
-    df_dias_actividades = calcula_promedio(df_merged)
+    # se calcular el total de km recorridos por ciclista
+    df_total_km = calcular_kilometros_totales(df_union)
 
-    # Une los datos finales
-    df_final = df_total_km.join(df_dias_actividades, ['Cedula', 'Nombre', 'Provincia'])\
+    # se calcula el promedio diario de kilómetros recorridos por ciclista
+    df_promedio_diario = calcular_promedio_diario(df_union)
+
+    # se une el total de km con el promedio diario
+    df_final = df_total_km.join(df_promedio_diario, ['Cedula', 'Nombre', 'Provincia'])\
                           .withColumn('Promedio_Diario', col('Kilometros_Totales') / col('Dias_Activos'))
 
-    # Obtiene el top 5 por km
-    df_top_5_km = top_5_ciclistas(df_final, 'Kilometros_Totales')
+    # top 5 por km totales
+    df_top_5_km = obtener_top_5(df_final, 'Kilometros_Totales')
     print("Top 5 ciclistas por provincia (kilómetros totales):")
     df_top_5_km.show()
 
-    # Obtiene el top 5 por promedio diario
-    df_top_5_prom = top_5_ciclistas(df_final, 'Promedio_Diario')
+    # se obtiene el top 5 por promedio diario
+    df_top_5_promedio = obtener_top_5(df_final, 'Promedio_Diario')
     print("\nTop 5 ciclistas por provincia (promedio diario):")
-    df_top_5_prom.show()
+    df_top_5_promedio.show()
 
 if __name__ == "__main__":
     main()
