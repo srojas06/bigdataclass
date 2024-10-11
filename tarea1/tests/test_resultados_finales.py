bacef19e6aad23f63a169d80d52fa77cd611e0e0
srojas06 <57544567+srojas06@users.noreply.github.com>
2024-10-10 19:04:02 -0600

    Update test_resultados_finales.py

--- tarea1/tests/test_resultados_finales.py
+++ tarea1/tests/test_resultados_finales.py
@@ -314,6 +314,95 @@ def test_un_solo_ciclista(spark_session):
 
     assert sorted(actual_rows, key=lambda x: x['Provincia']) == sorted(expected_rows, key=lambda x: x['Provincia'])
 
+#7 Test de verificar el ranking en todas las provincias 
+def test_multiples_provincias(spark_session):
+    # se crea un dataFrame con ciclistas de 7 provincias
+    df_actividades = spark_session.createDataFrame(
+        [
+            # San José
+            (118090887, 'Juan Perez', 'San José', '2024-10-01', 50.0),
+            (118090888, 'Carlos Mora', 'San José', '2024-10-01', 30.0),
+            (118090889, 'Javier Diaz', 'San José', '2024-10-01', 90.0),
+            (118090890, 'Sofía Alvarado', 'San José', '2024-10-01', 60.0),
+            (118090891, 'María López', 'San José', '2024-10-01', 80.0),
+            # Heredia
+            (123456789, 'Maria Gomez', 'Heredia', '2024-10-01', 20.0),
+            (123456780, 'Isabella Cruz', 'Heredia', '2024-10-01', 40.0),
+            (123456781, 'Luis Hernández', 'Heredia', '2024-10-01', 55.0),
+            (123456782, 'Lucía Gómez', 'Heredia', '2024-10-01', 50.0),
+            (123456783, 'Daniela López', 'Heredia', '2024-10-01', 25.0),
+            # Alajuela
+            (222222222, 'Diego Sánchez', 'Alajuela', '2024-10-01', 85.0),
+            (222222223, 'Laura Méndez', 'Alajuela', '2024-10-01', 40.0),
+            (222222224, 'Isabel Torres', 'Alajuela', '2024-10-01', 55.0),
+            (222222225, 'Miguel Ángel', 'Alajuela', '2024-10-01', 30.0),
+            (222222226, 'Ricardo Gómez', 'Alajuela', '2024-10-01', 60.0),
+            # Cartago
+            (333333333, 'José Martínez', 'Cartago', '2024-10-01', 70.0),
+            (333333334, 'Ana Torres', 'Cartago', '2024-10-01', 50.0),
+            (333333335, 'Pablo Pérez', 'Cartago', '2024-10-01', 45.0),
+            (333333336, 'Sofia Méndez', 'Cartago', '2024-10-01', 20.0),
+            (333333337, 'Diego Ruiz', 'Cartago', '2024-10-01', 30.0),
+            # Guanacaste
+            (444444444, 'Fernando Castro', 'Guanacaste', '2024-10-01', 60.0),
+            (444444445, 'Sofía Morales', 'Guanacaste', '2024-10-01', 90.0),
+            (444444446, 'Rafael López', 'Guanacaste', '2024-10-01', 80.0),
+            (444444447, 'María González', 'Guanacaste', '2024-10-01', 55.0),
+            (444444448, 'Laura Jiménez', 'Guanacaste', '2024-10-01', 50.0),
+            # Puntarenas
+            (555555555, 'Carlos Díaz', 'Puntarenas', '2024-10-01', 75.0),
+            (555555556, 'Isabel Jiménez', 'Puntarenas', '2024-10-01', 80.0),
+            (555555557, 'Diego Hernández', 'Puntarenas', '2024-10-01', 70.0),
+            (555555558, 'María Salas', 'Puntarenas', '2024-10-01', 30.0),
+            (555555559, 'Ana Rodríguez', 'Puntarenas', '2024-10-01', 40.0),
+            # Limón
+            (666666666, 'Lucía González', 'Limón', '2024-10-01', 65.0),
+            (666666667, 'Pedro López', 'Limón', '2024-10-01', 45.0),
+            (666666668, 'Carlos Arias', 'Limón', '2024-10-01', 50.0),
+            (666666669, 'Andrea Pérez', 'Limón', '2024-10-01', 30.0),
+            (666666670, 'Fernando Ruiz', 'Limón', '2024-10-01', 25.0),
+        ],
+        ['Cedula', 'Nombre', 'Provincia', 'Fecha', 'Kilometros']
+    )
+
+    # se calcula el total y promedio
+    df_top_n = df_actividades.groupBy("Cedula", "Nombre", "Provincia") \
+        .agg(F.sum("Kilometros").alias("Total_Kilometros")) \
+        .withColumn("Rank", F.row_number().over(Window.partitionBy("Provincia").orderBy(F.desc("Total_Kilometros")))) \
+        .filter(col("Rank") <= 3) \
+        .groupBy("Provincia") \
+        .agg(F.collect_list(struct("Rank", "Nombre", "Total_Kilometros")).alias("Top_Ciclistas")) \
+        .orderBy("Provincia")
+
+    print("Top 3 ciclistas por total de kilómetros:")
+    df_top_n.show()
+
+    # Datos esperados para el top 3 con ranking
+    expected_ds = spark_session.createDataFrame(
+        [
+            ('San José', [(1, 'Javier Diaz', 90.0), (2, 'María López', 80.0), (3, 'Sofía Alvarado', 60.0)]),
+            ('Heredia', [(1, 'Luis Hernández', 55.0), (2, 'Lucía Gómez', 50.0), (3, 'Isabella Cruz', 40.0)]),
+            ('Alajuela', [(1, 'Diego Sánchez', 85.0), (2, 'Isabel Torres', 55.0), (3, 'Ricardo Gómez', 60.0)]),
+            ('Cartago', [(1, 'José Martínez', 70.0), (2, 'Ana Torres', 50.0), (3, 'Pablo Pérez', 45.0)]),
+            ('Guanacaste', [(1, 'Sofía Morales', 90.0), (2, 'Fernando Castro', 60.0), (3, 'María González', 55.0)]),
+            ('Puntarenas', [(1, 'Carlos Díaz', 75.0), (2, 'Isabel Jiménez', 80.0), (3, 'Diego Hernández', 70.0)]),
+            ('Limón', [(1, 'Lucía González', 65.0), (2, 'Carlos Arias', 50.0), (3, 'Pedro López', 45.0)]),
+        ],
+        ['Provincia', 'Top_Ciclistas']
+    )
+
+    actual_rows = [row.asDict() for row in df_top_n.collect()]
+    expected_rows = [row.asDict() for row in expected_ds.collect()]
+
+    for actual in actual_rows:
+        actual['Top_Ciclistas'] = [(x[0], x[1], x[2]) for x in actual['Top_Ciclistas']]
+
+    for expected in expected_rows:
+        expected['Top_Ciclistas'] = [(x[0], x[1], x[2]) for x in expected['Top_Ciclistas']]
+
+    # Comparar resultados
+    assert sorted(actual_rows, key=lambda x: x['Provincia']) == sorted(expected_rows, key=lambda x
+
 
 
 if __name__ == "__main__":
@@ -324,4 +413,5 @@ if __name__ == "__main__":
     test_ranking_por_kilometros_totales(spark)
     test_cero_ciclistas(spark)
     test_un_solo_ciclista(spark)
+    test_multiples_provincias(spark)
     print("Todos los tests pasaron correctamente.")
