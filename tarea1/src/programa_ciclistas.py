68fe999440db4461e190be04160ae60976a99019
srojas06 <57544567+srojas06@users.noreply.github.com>
2024-10-10 21:15:43 -0600

    Update programa_ciclistas.py

--- tarea1/src/programa_ciclistas.py
+++ tarea1/src/programa_ciclistas.py
@@ -5,7 +5,7 @@ from pyspark.sql.window import Window
 
 spark = SparkSession.builder.appName("Ciclistas_Top5").getOrCreate()
 
-# Carga los CSV 
+# se carga los CSV 
 df_ciclistas = spark.read.csv('/src/archivo/ciclista.csv', header=False, inferSchema=True)\
     .toDF('Cedula', 'Nombre', 'Provincia')
 
@@ -19,25 +19,25 @@ df_actividades = spark.read.csv('/src/archivo/actividad.csv', header=False, infe
 df_merged = df_actividades.join(df_ciclistas, 'Cedula')\
                           .join(df_rutas, 'Codigo_Ruta')
 
-# Calcula los km totales por cada ciclista
+# se calcula los km totales por cada ciclista
 df_total_km = df_merged.groupBy('Cedula', 'Nombre', 'Provincia')\
                        .agg(_sum('Kilometros').alias('Kilometros_Totales'))
 
-# Calcular los dias en las que hay actividad
+# se calcula los dias en las que hay actividad
 df_dias_actividades = df_merged.groupBy('Cedula', 'Nombre', 'Provincia')\
                                .agg(countDistinct('Fecha').alias('Dias_Activos'))
 
-# Une las tablas con km totales y dias donde haya actividad 
+# se une las tablas con km totales y dias donde haya actividad 
 df_final = df_total_km.join(df_dias_actividades, ['Cedula', 'Nombre', 'Provincia'])\
                       .withColumn('Promedio_Diario', round(col('Kilometros_Totales') / col('Dias_Activos'), 2))
 
-# Crear una ventana de partición por provincia para el ranking
+# se crea una ventana de partición por provincia para el ranking
 windowSpec = Window.partitionBy('Provincia').orderBy(col('Kilometros_Totales').desc(), col('Promedio_Diario').desc())
 
-# Obtiene el top 5 de ciclistas por provincia
+# se obtiene el top 5 de ciclistas por provincia
 df_top_5 = df_final.withColumn("rank", rank().over(windowSpec))\
                    .filter(col("rank") <= 5)\
                    .drop("rank")
 
-# Muestra el top 5 de ciclistas por provincia
+# se muestra el top 5 de ciclistas por provincia
 df_top_5.show(35)
