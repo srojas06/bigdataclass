e9adc8a5b5674de8a84a3d8db8f700e4e86d2ef2
srojas06 <57544567+srojas06@users.noreply.github.com>
2024-10-16 15:18:18 -0600

    Update crear_db.sh

--- tarea2/puntosextras/db/crear_db.sh
+++ tarea2/puntosextras/db/crear_db.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+# Eliminar cualquier contenedor anterior llamado bigdata-db
+docker rm -f bigdata-db
 
+# Crear y ejecutar un contenedor PostgreSQL
+docker run --name bigdata-db \
+  -e POSTGRES_PASSWORD=testPassword \
+  -p 5433:5432 \
+  -d postgres
