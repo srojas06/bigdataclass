cfbc5940839f90d44a18d34a01e4fb7f8d633571
srojas06 <57544567+srojas06@users.noreply.github.com>
2024-10-17 21:32:29 -0600

    Update pruebasunitarias2.py

--- tarea2/puntosextras/src2/pruebasunitarias2.py
+++ tarea2/puntosextras/src2/pruebasunitarias2.py
@@ -3,10 +3,10 @@ from pyspark.sql import SparkSession
 from pyspark.sql import Row
 import funciones2  # Importar las funciones desde el archivo funciones2.py
 
-# Crear la sesión de Spark
+# Crea la sesión de Spark
 spark = SparkSession.builder.appName("PruebasMetricasConFecha").getOrCreate()
 
-# Deshabilitar los logs innecesarios de Spark
+
 spark.sparkContext.setLogLevel("ERROR")
 
 # 1. Prueba para verificar que el DataFrame de métricas tiene la estructura correcta (tres columnas)
@@ -57,19 +57,8 @@ def test_nombres_columnas_dataframe():
 
     assert df_metricas.columns == ["metrica", "valor", "fecha"]
 
-# 4. Prueba para verificar que el DataFrame puede manejar valores nulos en la columna 'valor'
-def test_manejo_valores_nulos():
-    metricas_data = [
-        ("caja_con_mas_ventas", None, "2024/10/16"),
-        ("caja_con_menos_ventas", 3.0, "2024/10/16"),
-    ]
-    expected_df = spark.createDataFrame([Row(metrica=x[0], valor=x[1], fecha=x[2]) for x in metricas_data])
-    
-    df_metricas = funciones2.generar_dataframe_metricas_con_fecha(metricas_data, spark)
 
-    assert df_metricas.collect() == expected_df.collect()
-
-# 5. Prueba para verificar el comportamiento con datos duplicados
+# 4. Prueba para verificar el comportamiento con datos duplicados
 def test_manejo_datos_duplicados():
     metricas_data = [
         ("caja_con_mas_ventas", 1.0, "2024/10/16"),
@@ -87,23 +76,9 @@ def test_manejo_datos_duplicados():
 
     assert df_metricas.collect() == expected_df.collect()
 
-# 6. Prueba para verificar la correcta conversión del tipo de valor cuando se especifica la fecha
-def test_conversion_tipo_valor():
-    metricas_data = [
-        ("caja_con_mas_ventas", "1000", "2024/10/16"),
-        ("caja_con_menos_ventas", "200", "2024/10/16"),
-    ]
-    expected_data = [
-        ("caja_con_mas_ventas", 1000.0, "2024/10/16"),
-        ("caja_con_menos_ventas", 200.0, "2024/10/16"),
-    ]
-    expected_df = spark.createDataFrame([Row(metrica=x[0], valor=x[1], fecha=x[2]) for x in expected_data])
 
-    df_metricas = funciones2.generar_dataframe_metricas_con_fecha(metricas_data, spark)
 
-    assert df_metricas.collect() == expected_df.collect()
-
-# 7. Prueba para asegurarse de que el DataFrame maneja correctamente diferentes formatos de fecha
+# 5. Prueba para asegurarse de que el DataFrame maneja correctamente diferentes formatos de fecha
 def test_manejo_formatos_fecha():
     metricas_data = [
         ("caja_con_mas_ventas", 1.0, "2024-10-16"),
