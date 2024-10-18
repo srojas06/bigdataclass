d416908a6581dc686a96cae585cd7d5bfa0bddfa
srojas06 <57544567+srojas06@users.noreply.github.com>
2024-10-17 21:58:10 -0600

    Update setup_postgres.sh

--- tarea2/puntosextras/setup_postgres.sh
+++ tarea2/puntosextras/setup_postgres.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-# Iniciar el servicio de PostgreSQL
+
 service postgresql start
 
-# Configurar PostgreSQL: crear base de datos, usuarios y establecer permisos
+# Configuramos PostgreSQL
 su - postgres -c "psql -c \"CREATE USER postgres WITH PASSWORD 'testPassword';\""
 su - postgres -c "psql -c \"ALTER USER postgres WITH SUPERUSER;\""
 su - postgres -c "psql -c \"CREATE DATABASE bigdata_db;\""
 
-# Modificar el archivo de configuración para permitir la autenticación
+# Modificamos el archivo de configuración para permitir la autenticacionnn
 echo "host all all 0.0.0.0/0 md5" >> /etc/postgresql/17/main/pg_hba.conf
 echo "listen_addresses='*'" >> /etc/postgresql/17/main/postgresql.conf
 
-# Reiniciar el servicio para aplicar los cambios
+# Reiniciamos el servicio para aplicar los cambios
 service postgresql restart
