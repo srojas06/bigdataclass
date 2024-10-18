f921f7e98c20719a265554b2559821478062eaef
srojas06 <57544567+srojas06@users.noreply.github.com>
2024-10-17 21:53:42 -0600

    Update programastephanie.py

--- tarea2/src/programastephanie.py
+++ tarea2/src/programastephanie.py
@@ -3,15 +3,15 @@ import shutil
 import glob
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
-import funciones  # Importar las funciones desde el archivo funciones.py
+import funciones  # Importa las funciones desde el archivo funciones.py
 
-# Crear la sesión de Spark
+# Crea la sesión de Spark
 spark = SparkSession.builder.appName("Tarea2BigData").getOrCreate()
 
-# Deshabilitar los logs innecesarios de Spark
+# Deshabilita los logs innecesarios de Spark
 spark.sparkContext.setLogLevel("ERROR")
 
-# Expandir el patrón 'caja*.yaml' automáticamente
+# hacer el 'caja*.yaml' automáticamente
 archivos_yamls = glob.glob(os.path.join("..", "data", "caja*.yaml"))
 
 if len(archivos_yamls) == 0:
@@ -25,29 +25,29 @@ for ruta in archivos_yamls:
     df = funciones.convertir_a_dataframe(datos_yaml, spark)
     dataframes.append(df)
 
-# Unir todos los DataFrames en uno solo
+# Une todos los DataFrames en uno solo
 df_final = dataframes[0]
 for df in dataframes[1:]:
     df_final = df_final.union(df)
 
-# Crear la columna total_venta (cantidad * precio_unitario)
+# Crea la columna total_venta (cantidad * precio_unitario)
 df_final = df_final.withColumn("total_venta", F.col("cantidad") * F.col("precio_unitario"))
 
-# Calcular el total de productos vendidos
+# Calcula el total de productos vendidos
 total_productos = df_final.groupBy("nombre_producto").agg(F.sum("cantidad").alias("cantidad_total"))
 
-# Mostrar el total de productos vendidos en el CMD
+# Muestra el total de productos vendidos 
 print("\n--- Total de productos vendidos ---")
 total_productos.show()
 
-# Calcular el total de ventas por caja
+# Calcula el total de ventas por caja
 total_cajas = df_final.groupBy("numero_caja").agg(F.sum("total_venta").alias("total_vendido"))
 
-# Mostrar el total vendido por caja en el CMD
+# Muestra el total vendido por caja 
 print("\n--- Total vendido por caja ---")
 total_cajas.show()
 
-# Calcular las métricas
+# Calcula las métricas
 caja_con_mas_ventas, caja_con_menos_ventas, percentil_25, percentil_50, percentil_75 = funciones.calcular_metricas(df_final)
 producto_mas_vendido, producto_mayor_ingreso = funciones.calcular_productos(df_final)
 
@@ -64,28 +64,28 @@ metricas_data = [
 
 df_metricas = spark.createDataFrame(metricas_data, ["Métrica", "Valor"])
 
-# Mostrar las métricas como una tabla en el CMD
+# Muestra las métricas como una tabla
 print("\n--- Métricas ---")
 df_metricas.show()
 
-# Función para eliminar la carpeta si ya existe
+# funcion que elimina la carpeta si ya existe
 def eliminar_carpeta_si_existe(ruta_carpeta):
     if os.path.exists(ruta_carpeta):
         shutil.rmtree(ruta_carpeta)
 
-# Eliminar las carpetas si ya existen para sobrescribir
+# Elimina las carpetas 
 eliminar_carpeta_si_existe("/src/output/total_productos")
 eliminar_carpeta_si_existe("/src/output/total_cajas")
 eliminar_carpeta_si_existe("/src/output/metricas")
 
-# Guardar los resultados de total_productos en una carpeta y un archivo
+# Guarda los resultados de total_productos en una carpeta y un archivo
 total_productos.coalesce(1).write.mode("overwrite").csv("/src/output/total_productos", header=True)
 
-# Guardar los resultados de total_cajas en una carpeta y un archivo
+# Guarda los resultados de total_cajas en una carpeta y un archivo
 total_cajas.coalesce(1).write.mode("overwrite").csv("/src/output/total_cajas", header=True)
 
-# Guardar las métricas en la carpeta "metricas" con un solo archivo
+# Guarda las métricas en la carpeta "metricas" con un solo archivo
 df_metricas.coalesce(1).write.mode("overwrite").csv("/src/output/metricas", header=True)
 
-# Finalizar la sesión de Spark
+# Finaliza la sesión de Spark
 spark.stop()
