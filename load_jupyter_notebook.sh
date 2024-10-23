f46829441e2a0df7558c0d432864eb6f698c3ffa
srojas06 <57544567+srojas06@users.noreply.github.com>
2024-10-23 02:17:23 -0600

    Update load_jupyter_notebook.sh

--- load_jupyter_notebook.sh
+++ load_jupyter_notebook.sh
@@ -3,4 +3,4 @@
 # It will prompt the user to load up a browser on their own using a URL that
 # contains a token. Because of the port mapping configured upon running the
 # image it should be possible to use that URL from the host machine.
-jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser
+jupyter notebook --ip=0.0.0.0 --port=8888 
